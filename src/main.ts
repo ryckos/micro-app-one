77df1ca7aa7788697f2ecbc439f5cfcca8e05b6f
SEDZRO Derick <sedzroderic@gmail.com>
2021-12-09 19:06:05 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import './set-public-path';
+import Vue from 'vue';
+import singleSpaVue from 'single-spa-vue';
+
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+const containerSelector = '#appPlace'
+
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render(h: any) {
+      return h(App, {
+        props: {
+          // single-spa props are available on the "this" object. Forward them to your component as needed.
+          // https://single-spa.js.org/docs/building-applications#lifecyle-props
+          // if you uncomment these, remember to add matching prop definitions for them in your App.vue file.
+          /*
+          name: this.name,
+          mountParcel: this.mountParcel,
+          singleSpa: this.singleSpa,
+          */
+        },
+      });
+    },
+    el: containerSelector
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
+
+export const devtools = {
+  overlays: {
+    selectors: [
+      containerSelector
+    ],
+  }
+};
